9d6d78f116b98716b7c29ccd5cff0e0a6c029a2e
Eugene Molari <eugelogic@gmail.com>
2017-05-29 15:40:04 +0100

    Turn header dynamic

--- themes/hlcchl/header.php
+++ themes/hlcchl/header.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * The header for our theme
- *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
@@ -23,27 +22,45 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'hlcchl' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+<!-- HEADER
+================================================== -->
+<header class="site-header" role="banner">
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+  <!-- TOP BAR
+  ================================================== -->
+  <section id="top-bar">
+    <div class="container">
+      Call Us on
+      <a href="tel:02083429524">0208 342 9524</a>
+      -
+      Send Us an Email <a href="mailto:hollylodgelondon@hotmail.com">hollylodgelondon@hotmail.com</a>
+    </div><!-- container -->
+  </section>
+
+  <!-- SITE TITLE
+  ================================================== -->
+  <section id="site-title">
+    <div class="container">
+      <h1><?php bloginfo('name'); ?></h1>
+    </div><!-- container -->
+  </section>
+
+  <!-- MAIN NAV MENU
+  ================================================== -->
+  <nav>
+    <div class="container">
+			<!-- If the menu (WP admin area) is not set, then the "menu_class" is applied to "container". -->
+			<!-- In other words, it overwrites the "container_class". -->
+			<!-- Ref: http://wordpress.org/support/topic/wp_nav_menu-menu_class-usage-bug?replies=4 -->
 			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+				wp_nav_menu( array(
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hlcchl' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+					'theme_location'	=> 'primary',
+					'container'				=> 'nav'
 
-	<div id="content" class="site-content">
+				) );
+			?>
+    </div><!-- container -->
+  </nav>
+
+</header>
